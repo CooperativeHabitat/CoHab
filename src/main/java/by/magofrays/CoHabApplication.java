7ca669a5a1febefab82410a3536a126ab0ec9057
magofrays <vadimpozdeev2005@gmail.com>
2026-06-18 11:26:26 +0300

    application fix

--- src/main/java/by/magofrays/CoHabApplication.java
+++ src/main/java/by/magofrays/CoHabApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoHabApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         SpringApplication.run(CoHabApplication.class, args);
     }
